6fba4e683a9fa6d1161b4cb96127a8622256ffed
Dani <dani@shiryu.(none)>
2012-12-09 09:23:01 +0100

    Updated licensing information to my real name

--- jquery.ui.inputimg.js
+++ jquery.ui.inputimg.js
@@ -1,5 +1,5 @@
 /*
-  Copyright (c) 2012 dncrht
+  Copyright (c) 2012 Daniel Cruz Horts
 
   Permission is hereby granted, free of charge, to any person obtaining a copy
   of this software and associated documentation files (the "Software"), to deal
